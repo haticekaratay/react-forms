a3eecc52a55cf3ce0bb063e249170560859d7cf7
Hatice <haticekaraty@gmail.com>
2021-09-13 23:45:05 -0400

    commit tree 1d363c5f61342fd32424cf49c9c6d95ba6c1bbbd

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,31 +1,65 @@
-import React from 'react';
-
-class Form extends React.Component {
-  state = {
-    firstName: "John",
-    lastName: "Henry"
+import React, {useState} from "react"
+function Form(){
+  const [firstName, setfirstName] = useState("John");
+  const [lastName, setlastName] = useState("Henry");
+  
+  function handleFirstNameChange(event){
+      setfirstName(event.target.value)
   }
 
-  handleFirstNameChange = event => {
-    this.setState({
-      firstName: event.target.value
-    })
+  function handleLastNameChange(event){
+     setlastName(event.target.value)
   }
 
-  handleLastNameChange = event => {
-    this.setState({
-      lastName: event.target.value
-    })
-  }
-
-  render() {
-    return (
-      <form>
-        <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
-        <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
-      </form>
-    )
-  }
+  return(
+    <form>
+      <input
+        type="text"
+        onChange={handleFirstNameChange}
+        value={firstName}
+      />
+      <input
+        type="text"
+        onChange={handleLastNameChange}
+        value={lastName}
+      />
+      <button type="submit">Submit</button>
+    </form>
+  );
 }
-
 export default Form;
+
+
+
+//class Component version
+// import React from 'react';
+
+// class Form extends React.Component {
+//   state = {
+//     firstName: "John",
+//     lastName: "Henry"
+//   }
+
+//   handleFirstNameChange = event => {
+//     this.setState({
+//       firstName: event.target.value
+//     })
+//   }
+
+//   handleLastNameChange = event => {
+//     this.setState({
+//       lastName: event.target.value
+//     })
+//   }
+
+//   render() {
+//     return (
+//       <form>
+//         <input type="text" name="firstName" onChange={event => this.handleFirstNameChange(event)} value={this.state.firstName} />
+//         <input type="text" name="lastName" onChange={event => this.handleLastNameChange(event)} value={this.state.lastName} />
+//       </form>
+//     )
+//   }
+// }
+
+// export default Form;
